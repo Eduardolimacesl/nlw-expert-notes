089448c664641605178d7996a42da3720fe6dd96
@eduardoLima <eduardoLimacesl@gmail.com>
2024-02-07 09:59:59 -0300

    feat: add modal into see note using Dialog Portal

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,12 @@
 import logo from './assets/logo-nlw-experts.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
+
+const note = {
+  date: new Date(),
+  content: "Hello World"
+}
+
 export default function App() {
   return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
@@ -22,11 +28,8 @@ export default function App() {
 
       <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+
+        <NoteCard note={note}/>
       </div>
     </div>
   )
